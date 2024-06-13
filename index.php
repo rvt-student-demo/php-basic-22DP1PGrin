4f1558ff4b4a9606b8941fe43102f824202ba87b
22DP1PGrin <131352980+22DP1PGrin@users.noreply.github.com>
2024-06-13 12:18:45 +0000

    Exsercises are done

--- index.php
+++ index.php
@@ -15,7 +15,27 @@ if($z%2==0){
 }else{
     echo "\nThe number ".$z." is odd!";
 }
-// 5. uzdevums
+// 5. uzdevums 
 $numbers = array(20,13,89,5,10);
+echo "\nIn array:";
+foreach($numbers as $x){
+    echo $x.", ";
+}
 echo"\nThe min number in array is ".(min($numbers)).", but max number is ".(max($numbers)).".";
+// 6. uzdevums
+$String="Madam";
+echo"\nThe reverse '".$String."' is '".strrev($String)."'.";
+// 7. uzdevums
+if(strtolower($String)==strtolower(strrev($String))){
+    echo "This word is palindrome!";
+}else{
+    echo "This word isn't palindrome! ";
+}
+// 8. uzdevums
+echo "\nThe random number is: ".rand(1,10);
+// 9. uzdevums
+$SomeWords="I like cartoons dogs";
+echo"\n".strtoupper($SomeWords);
+// 10. uzdevums
+echo ". In this sentence is ".str_word_count($SomeWords)." words.";
 ?>
